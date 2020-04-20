d2900ebffd9800d8d6339ee14f82f5cff3252299
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-04-20 10:38:07 +0100

    Add create to feedback controller

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -4,6 +4,22 @@ class FeedbackController < ApplicationController
   skip_before_action :authenticate_user!
   skip_authorization_check
   
-  def new; end
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new feedback_params
+    if @feedback.valid?
+      redirect_to new_feedback_path, notice: 'Your feedback has been submitted'
+    else
+      render new_feedback_path
+    end
+  end
+
+  private
+  def feedback_params
+    params.require(:feedback).permit(:comment, :email, :rating)
+  end
 end
   
